4056453c303cb432cd941b5efd558a6e7832096f
Bruno Coutinho <bcoutinhoti@gmail.com>
2025-06-02 09:16:47 -0300

    Add files via upload

---
+++ ExemploJDBC/src/main/java/br/com/rpires/dao/generic/jdbc/dao/IProdutoDAO.java
@@ -0,0 +1,18 @@
+package br.com.rpires.dao.generic.jdbc.dao;
+
+import br.com.rpires.dominio.Produto;
+import java.util.List;
+
+public interface IProdutoDAO {
+
+    Integer cadastrar(Produto produto) throws Exception;
+
+    Integer atualizar(Produto produto) throws Exception;
+
+    Produto buscar(String codigo) throws Exception;
+
+    List<Produto> buscarTodos() throws Exception;
+
+    Integer excluir(Produto produto) throws Exception;
+}
+
